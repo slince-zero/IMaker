44fd299f600a174658396d82c7d84906606ff1e7
slince-zero <1711257704@qq.com>
2024-04-22 17:14:29 +0800

    center init

---
+++ src/page/center/index.tsx
@@ -0,0 +1,3 @@
+export default function CenterBoard() {
+  return <div className='max-h-screen  flex  rounded-3xl'>222ss</div>
+}
--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,12 +1,15 @@
 // main page
 import LeftBoard from '@/page/left'
+import CenterBoard from '@/page/center'
 export default function Main() {
   return (
     <div className='conatainer flex h-screen'>
       <div className='left w-[20%] '>
         <LeftBoard />
       </div>
-      <div className='center w-[60%] '>center</div>
+      <div className='center w-[60%] '>
+        <CenterBoard />
+      </div>
       <div className='right w-[20%] '>right</div>
     </div>
   )
