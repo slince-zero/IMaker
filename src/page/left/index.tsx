9301f0b1eb8d87430a995a7254e6112dd6da330f
slince-zero <1711257704@qq.com>
2024-04-23 15:10:16 +0800

    left 搜索功能完成

--- src/page/left/index.tsx
+++ src/page/left/index.tsx
@@ -10,27 +10,54 @@ import {
 } from '@nextui-org/react'
 import { ImgLogo, UploadLogo } from './logo'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, KeyboardEvent } from 'react'
 import ImageItem from './ImageItem'
 export default function LeftBoard() {
   const [imageList, setImageList] = useState<ImageItem[]>([])
   const [searchValue, setSearchValue] = useState('')
   const [isLoading, setIsLoading] = useState(false)
 
-  // let key="cPSLU8ro2LKkaLQ71-RKKFFwb2g-_DIx9NCf27YnCHs"
-
-  // https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
-
-  async function getImage() {
-    const res = await fetch(
-      'https://api.unsplash.com/photos?per_page=30&client_id=cPSLU8ro2LKkaLQ71-RKKFFwb2g-_DIx9NCf27YnCHs'
-    )
-    const data = await res.json()
-    setImageList(data)
+  // 获取图片
+  async function getImage(searchText: string = '') {
+    try {
+      setIsLoading(true)
+      const accessKey = 'cPSLU8ro2LKkaLQ71-RKKFFwb2g-_DIx9NCf27YnCHs'
+      const endpoint =
+        searchText.length > 0
+          ? `https://api.unsplash.com/search/photos`
+          : `https://api.unsplash.com/photos`
+      const queryParam = searchText
+        ? `&query=${encodeURIComponent(searchText)}`
+        : ''
+      const res = await fetch(
+        `${endpoint}?per_page=30${queryParam}&client_id=${accessKey}`
+      )
+      const data = await res.json()
+      if (data && data.results) {
+        // 如果是使用搜索API，结果会在data.results中
+        setImageList(data.results)
+      } else {
+        setImageList(data) // 直接访问 Unsplash collections/photos 数据在顶层
+      }
+      console.log(data);
+      
+    } catch (e) {
+      console.error(e)
+    } finally {
+      setIsLoading(false)
+    }
   }
+
   useEffect(() => {
     getImage()
   }, [])
+
+  // 按下回车事件
+  function onSearchKeyDown(e: KeyboardEvent<HTMLInputElement> | KeyboardEvent) {
+    if (e.key === 'Enter') {
+      getImage(searchValue)
+    }
+  }
 
   return (
     <div className='flex flex-col bg-slate-500 h-screen'>
@@ -54,16 +81,22 @@ export default function LeftBoard() {
 
       <ScrollShadow>
         <div className='flex-grow overflow-y-scroll overflow-x-hidden justify-center flex flex-wrap scrollbar-thin scrollbar-color-auto '>
-          {imageList.map((item) => {
-            return (
-              <img
-                key={item?.id}
-                src={item?.urls.small}
-                alt={item?.alt_description}
-                className='transition-transform duration-200 transform hover:scale-105 rounded m-2 cursor-pointer w-5/12 object-cover h-24'
-              />
-            )
-          })}
+          {imageList.length == 0 ? (
+            <div className='flex items-center justify-center  transition-transform duration-200 transform hover:scale-105 rounded m-2 cursor-pointer w-5/12 object-cover h-screen'>
+              没有找到图片
+            </div>
+          ) : (
+            imageList.map((item) => {
+              return (
+                <img
+                  key={item?.id}
+                  src={item?.urls.small}
+                  alt={item?.alt_description}
+                  className='transition-transform duration-200 transform hover:scale-105 rounded m-2 cursor-pointer w-5/12 object-cover h-24'
+                />
+              )
+            })
+          )}
         </div>
       </ScrollShadow>
 
@@ -79,11 +112,19 @@ export default function LeftBoard() {
             type='search'
             color='default'
             placeholder='请输入内容搜索图片'
+            value={searchValue}
+            onValueChange={setSearchValue}
+            onKeyDown={(e) => onSearchKeyDown(e)}
           />
 
           <NavbarContent justify='end'>
             <NavbarItem>
-              <Button>搜索</Button>
+              <Button
+                isLoading={isLoading}
+                isIconOnly
+                onClick={() => getImage(searchValue)}>
+                搜索
+              </Button>
             </NavbarItem>
           </NavbarContent>
         </Navbar>
