171fed3a68537aac1c702acc0d248fe40fd9fa71
slince-zero <1711257704@qq.com>
2024-04-23 11:01:22 +0800

    left right 布局样式基本完成，样式还需要做一些调整

--- src/page/right/index.tsx
+++ src/page/right/index.tsx
@@ -10,10 +10,32 @@ import {
   SelectItem,
   Slider,
   Textarea,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Tabs,
+  Tab,
+  Divider,
 } from '@nextui-org/react'
+import { useState } from 'react'
+import { CirclePicker } from 'react-color'
+import {
+  BottomLeftIcon,
+  BottomRightIcon,
+  MiddleIcon,
+  TopLeftIcon,
+  ToprightIcon,
+} from './posotion-logo'
 
 export default function RightBoard() {
+  const [hexColor, setHexColor] = useState('')
   const animals = ['cat', 'dog', '22']
+
+  function handleChangeCompleteColor(color: any) {
+    setHexColor(color.hex.toUpperCase())
+  }
+
   return (
     <div className='flex flex-col bg-slate-500 h-screen'>
       <>
@@ -53,16 +75,62 @@ export default function RightBoard() {
             </SelectItem>
           ))}
         </Select>
+        <Divider />
 
+        {/* 遮罩+颜色选择 */}
+        <div className='flex w-full items-center justify-between'>
+          <div className='w-4/5'>
+            <Input label='遮罩' />
+          </div>
+
+          <div className='mx-2'>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button
+                  isIconOnly
+                  size='lg'
+                  variant='bordered'></Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                aria-label='Static Actions'
+                variant='flat'
+                disallowEmptySelection
+                selectionMode='single'>
+                <DropdownItem textValue='color'>
+                  <div className='m-2'>
+                    <CirclePicker
+                      colors={[
+                        '#1f2937',
+                        '#e91e63',
+                        '#9c27b0',
+                        '#673ab7',
+                        '#3f51b5',
+                        '#2196f3',
+                        '#03a9f4',
+                        '#00bcd4',
+                        '#009688',
+                        '#4caf50',
+                        '#8bc34a',
+                        '#cddc39',
+                      ]}
+                      onChangeComplete={handleChangeCompleteColor}
+                    />
+                  </div>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        </div>
         <Slider
           color='foreground'
           label='透明度'
           step={1}
           maxValue={100}
           minValue={0}
-          defaultValue={0.4}
+          defaultValue={40}
           className='max-w-md'
         />
+        <Divider />
 
         <Select
           label='字体'
@@ -88,10 +156,24 @@ export default function RightBoard() {
           ))}
         </Select>
 
+        <Tabs
+          className='w-full'
+          classNames={{
+            tabList: 'w-full',
+          }}
+          aria-label='Options'>
+          <Tab title={<BottomLeftIcon />} />
+          <Tab title={<BottomRightIcon />} />
+          <Tab title={<MiddleIcon />} />
+          <Tab title={<TopLeftIcon />} />
+          <Tab title={<ToprightIcon />} />
+        </Tabs>
+        <Divider />
+
         <Textarea
           label='标题'
           placeholder='输入标题'
-          className='max-w-xs py-2'
+          className=' py-2'
         />
 
         <Input
@@ -101,6 +183,23 @@ export default function RightBoard() {
           placeholder='输入作者'
         />
       </div>
+      <Divider />
+
+      <>
+        <Navbar>
+          <NavbarBrand>
+            <span>下载图像</span>
+          </NavbarBrand>
+
+          <NavbarContent justify='end'>
+            <NavbarItem>
+              <Button>JPG</Button>
+              <Button className='mx-2'>PNG</Button>
+              <Button>SVG</Button>
+            </NavbarItem>
+          </NavbarContent>
+        </Navbar>
+      </>
     </div>
   )
 }
---
+++ src/page/right/posotion-logo.tsx
@@ -0,0 +1,114 @@
+export const BottomLeftIcon = () => (
+  <svg
+    viewBox='0 0 24 24'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    strokeWidth='0.125em'
+    className='css-z61snw'
+    width='24'
+    height='24'
+    fill='none'>
+    <path
+      d='M10 21H21V3L3 3L3 14'
+      stroke='currentcolor'
+      strokeLinecap='round'
+      strokeLinejoin='round'></path>
+    <circle
+      cx='4.5'
+      cy='19.5'
+      r='2.5'
+      stroke='currentcolor'></circle>
+  </svg>
+)
+
+export const BottomRightIcon = () => (
+  <svg
+    viewBox='0 0 24 24'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    strokeWidth='0.125em'
+    className='css-z61snw'
+    width='24'
+    height='24'
+    fill='none'>
+    <path
+      d='M21 14V3L3 3L3 21H14'
+      stroke='currentcolor'
+      strokeLinecap='round'
+      strokeLinejoin='round'></path>
+    <circle
+      cx='19.5'
+      cy='19.5'
+      r='2.5'
+      stroke='currentcolor'></circle>
+  </svg>
+)
+
+export const MiddleIcon = () => (
+  <svg
+    viewBox='0 0 24 24'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    strokeWidth='0.125em'
+    className='css-z61snw'
+    width='24'
+    height='24'
+    fill='none'>
+    <path
+      d='M3 3H21V21L3 21V3Z'
+      stroke='currentcolor'
+      strokeLinecap='round'
+      strokeLinejoin='round'></path>
+    <circle
+      cx='12'
+      cy='12'
+      r='2.5'
+      stroke='currentcolor'></circle>
+  </svg>
+)
+
+export const TopLeftIcon = () => (
+  <svg
+    viewBox='0 0 24 24'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    strokeWidth='0.125em'
+    className='css-z61snw'
+    width='24'
+    height='24'
+    fill='none'>
+    <path
+      d='M3 10L3 21H21V3L10 3'
+      stroke='currentcolor'
+      strokeLinecap='round'
+      strokeLinejoin='round'></path>
+    <circle
+      cx='4.5'
+      cy='4.5'
+      r='2.5'
+      stroke='currentcolor'></circle>
+  </svg>
+)
+
+export const ToprightIcon = () => (
+  <svg
+    viewBox='0 0 24 24'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    strokeWidth='0.125em'
+    className='css-z61snw'
+    width='24'
+    height='24'
+    fill='none'>
+    <path
+      d='M14 3H3L3 21H21V10'
+      stroke='currentcolor'
+      strokeLinecap='round'
+      strokeLinejoin='round'></path>
+    <circle
+      cx='19.5'
+      cy='4.5'
+      r='2.5'
+      stroke='currentcolor'></circle>
+  </svg>
+)
