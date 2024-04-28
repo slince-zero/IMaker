dc4382813c767ccbf826133a60d6dc41805b08ce
slince-zero <1711257704@qq.com>
2024-04-28 16:51:11 +0800

    完成键盘对画笔的撤销操作

--- src/page/right/hand-writing.tsx
+++ src/page/right/hand-writing.tsx
@@ -1,10 +1,12 @@
-import { useState, useRef, useContext } from 'react'
+import { useState, useRef, useContext, useEffect } from 'react'
 import { Stage, Layer, Line } from 'react-konva'
 import { ImgContext } from '@/context'
 
 interface LineInfo {
   boardTool: string
   points: number[]
+  color: string
+  strokeWidth: number
 }
 
 interface ContextProp {
@@ -22,7 +24,15 @@ function HandWriting() {
   const handleMouseDown = (e: any) => {
     isDrawing.current = true
     const pos = e.target.getStage().getPointerPosition()
-    setLines([...lines, { boardTool, points: [pos.x, pos.y] }])
+    setLines([
+      ...lines,
+      {
+        boardTool,
+        points: [pos.x, pos.y],
+        color: board_pen_color,
+        strokeWidth: penSize,
+      },
+    ])
   }
 
   const handleMouseMove = (e: any) => {
@@ -44,6 +54,36 @@ function HandWriting() {
     isDrawing.current = false
   }
 
+  // 撤销逻辑
+  useEffect(() => {
+    // 监听键盘事件
+    const handleKeyDown = (e: KeyboardEvent) => {
+      // 判断一下电脑系统
+      const userAgent = navigator.userAgent.toLowerCase()
+
+      if (userAgent.indexOf('windows') > -1) {
+        // Windows系统
+        if (e.ctrlKey && e.key === 'z') {
+          // 移除最后一条线并更新状态
+          setLines(lines.slice(0, lines.length - 1))
+        }
+      } else if (userAgent.indexOf('mac') > -1) {
+        // Mac系统
+        if (e.metaKey && e.key === 'z') {
+          // 如果'command'键和'z'键同时被按下，移除最后一条线并更新状态
+          setLines(lines.slice(0, lines.length - 1))
+        }
+      }
+    }
+    // 绑定键盘事件监听器
+    window.addEventListener('keydown', handleKeyDown)
+
+    // 清楚监听器
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [lines])
+
   return (
     <div>
       <Stage
@@ -57,8 +97,8 @@ function HandWriting() {
             <Line
               key={i}
               points={line.points}
-              stroke={board_pen_color}
-              strokeWidth={Number(penSize)}
+              stroke={line.color}
+              strokeWidth={Number(line.strokeWidth)}
               tension={0.5}
               lineCap='round'
               lineJoin='round'
