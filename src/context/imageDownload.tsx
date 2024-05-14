e46f436857b5be6a56b5ef346fac2379bbe16dd3
slince-zero <1711257704@qq.com>
2024-05-14 17:17:33 +0800

    修改下载图片名称 (IMaker-时间)

--- src/context/imageDownload.tsx
+++ src/context/imageDownload.tsx
@@ -74,8 +74,23 @@ export const ImageDownloadProvider = ({
             return
         }
 
+        function generateImageName(
+          prefix: string,
+          type: 'JPG' | 'PNG' | 'SVG'
+        ) {
+          const now = new Date()
+          const year = now.getFullYear()
+          const month = String(now.getMonth() + 1).padStart(2, '0')
+          const day = String(now.getDate()).padStart(2, '0')
+          const hours = String(now.getHours()).padStart(2, '0')
+          const minutes = String(now.getMinutes()).padStart(2, '0')
+          const seconds = String(now.getSeconds()).padStart(2, '0')
+          return `${prefix}-${year}${month}${day}-${hours}${minutes}${seconds}.${type.toLowerCase()}`
+        }
+
+        const imageName = generateImageName('IMAKER', type)
         const link = document.createElement('a')
-        link.download = `image.${type.toLowerCase()}`
+        link.download = imageName
         link.href = dataUrl
         link.click()
       } catch (err) {
